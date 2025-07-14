ed8b082ebce2fc2ccefe3eb49d38e6d789c8af67
sans 312a3 <ianbravob312@gmail.com>
2025-07-14 15:08:46 -0400

    ADD ID

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const formulario = document.querySelector(".form-create");
-
+let contador = 0;
 formulario.addEventListener("submit", (e) => {
     e.preventDefault();
     const publicacion = document.querySelector("#publicacion").value;
     const contenedor = document.querySelector(".contenedor-publicaciones");
 
     contenedor.innerHTML += `        <div class="publicacion">
-            <p>id: 00</p>
+            <p>${contador += 1}</p>
             <p>${publicacion}</p>
             <button>editar</button>
             <button>eliminar</button>
